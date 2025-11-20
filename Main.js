f89d3e0b7ffdad293705903088f745e57468ca7c
Arthur Biard <arthur.biard@cesjb.be>
2025-11-20 15:59:27 +0100

    enfiiiiin

--- Main.js
+++ Main.js
@@ -1,1 +1,4 @@
 import Etudiant from Etudiant.js
+var etudiant1 = new Etudiant ("Arthur", 18, "pro_gamer")
+var etudiant2 = new Etudiant ("Pierre", 1348, "doyen")
+var etudiant3 = new Etudiant ("David", 58, "harceleur")
